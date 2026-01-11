ec4cb6fbc43e944e1d6236e85550b54dfbb93d1d
suhaff <numaanhussain121@gmail.com>
2026-01-11 14:10:35 +0800

    Made the Database savable

---
+++ src/main/java/com/artify/service/AuthService.java
@@ -0,0 +1,25 @@
+package com.artify.service;
+
+import com.artify.model.User;
+import com.artify.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email).orElse(null);
+
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+
+        return null;
+    }
+}
